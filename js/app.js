78b0689b7d136170ced701791a5c178124c966a4
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-04-06 14:05:40 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const eye = document.getElementById("eye");
+const form = document.querySelector(".form__box");
+
+eye.addEventListener("click", ()=> {
+  form.style.backgroundColor = "#fff";
+});
---
+++ js/main.js
@@ -0,0 +1,106 @@
+// Problem 1
+
+
+// let massArray = [10, 1, 3, 5, 8, 9];
+
+// for(let i = 0; i < massArray.length; i++){
+//   for(let j = 0; j < massArray.length; j++){
+//     if(massArray[j] > massArray[j + 1]){
+//       let a = massArray[j + 1];
+//       massArray[j + 1] = massArray[j];
+//       massArray[j] = a;
+//     }
+//   }
+// }
+
+// console.log(massArray);
+
+// Problem 2
+
+// let object = [
+//   {
+//     login : "uzb",
+//     pass : "forever123",
+//   }
+// ];
+
+// object.forEach((item) => {
+//   if(item.login.trim().length < 3 && item.pass.trim().length < 8){
+//     console.log("Login kamida 3 belgidan iborat bo'lishi kerak");
+//     console.log("Parol kamida 8 belgidan iborat bo'lishi kerak");
+//   }
+//   else if(item.login.trim().length < 3){
+//     console.log("Login kamida 3 belgidan iborat bo'lishi kerak");
+//   } else if(item.pass.trim().length < 8){
+//     console.log("Parol kamida 8 belgidan iborat bo'lishi kerak");
+//   } else{
+//     console.log(`Siz muvaffaqqiyatli ro'yxatdan o'tdingiz \n Login: ${item.login} \n Parol: ${item.pass}`);
+//   }
+// });
+
+// Problem 4
+
+// let n = 10;
+// let k = 2;
+// let count = 0;
+
+// for(let i = 1; i <= n; i++){ 
+//   if(i > 1){
+//     let sum = 1;
+//     for(let j = 0; j < k; j++){
+//       sum *= i;
+//     }
+//     count += sum;
+//   } else if(i == 1){
+//     count += i;
+//   }
+// }
+
+// console.log(count);
+
+
+// let masArray = [7, 10, 3, 2];
+// let arr = "";
+// for(let i = 0; i < 2; i++){
+//   masArray.push(masArray[i])
+//   masArray.shift(masArray[i])
+// }
+// console.log(masArray); 
+
+
+// Problem 6
+
+// let masArray = [7, 10, 3, 2];
+// let arr = Math.floor(masArray.length / 2);
+// let count = arr;
+
+// for(let i = 0; i < arr; i++){
+//   let sum = masArray[i];
+//   masArray[i] = masArray[count];
+//   masArray[count] = sum;
+//   count++;
+// }
+
+// console.log(masArray);
+
+//Problem 5
+
+// let elArr = [1, [2, 3], undefined, {x: 1}];
+// let elNewArr = [];
+
+// for(let i = 0; i < elArr.length; i++){
+//   if(Number.isInteger(elArr[i])){
+//     elNewArr.push(elArr[i]);
+//   } else if (Array.isArray(elArr[i])){
+//     elArr[i].forEach((item) => {
+//       elNewArr.push(item);
+//     })
+//   } else if(typeof elArr[i] === "object"){
+//       elNewArr.push(Object.keys(elArr[i])[0]);
+//       elNewArr.push(Object.values(elArr[i])[0]);
+//   } else{
+//     elNewArr.push(elArr[i]);
+//   }
+// }
+
+// console.log(elNewArr);
